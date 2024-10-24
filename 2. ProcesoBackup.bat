250563558943077d0f201f35ca48d4d074fd1586
Sor1914 <91400444+Sor1914@users.noreply.github.com>
2024-10-24 00:05:15 -0600

    Actualizacion de modulos

--- 2. ProcesoBackup.bat
+++ 2. ProcesoBackup.bat
@@ -1,7 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-
-:: Configuración de Variables
 set "PG_DUMP_PATH=C:\Program Files\Odoo 17.0.20240822\PostgreSQL\bin\pg_dump.exe"
 set "BACKUP_DIR=C:\OdooBackups\central"
 set "DB_USER=openpg"
@@ -10,17 +8,10 @@ set "DB_NAME=Odoo"
 set "PGHOST=localhost"
 set "PGPORT=5432"
 set "DATE=%date:~10,4%-%date:~7,2%-%date:~4,2%"
-
-:: Crear el directorio de backup si no existe
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
-
-:: Realizar el backup incluyendo estructura y datos de las tablas específicas
 echo Haciendo backup de la base de datos %DB_NAME%...
 set "BACKUP_FILE=%BACKUP_DIR%\%DB_NAME%_backup_%DATE%.backup"
-
-:: Realizar el backup de estructura y datos de las tablas seleccionadas
 "%PG_DUMP_PATH%" -U %DB_USER% -h %PGHOST% -p %PGPORT% -d %DB_NAME% -F c -f "%BACKUP_FILE%" -t sale_order -t sale_order_line -t sale_report -t product_product -t product_template -t res_partner -t account_invoice -t account_move -t account_move_line -t stock_picking -t stock_move -t stock_quant
-
 if %errorlevel% neq 0 (
     echo Error durante el backup. Revisa el archivo de log: C:\backups\backup_error.log
 ) else (
